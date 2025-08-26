3cf6828e62d109cb0c795c5db390eb298ad20ae6
Ruben Manuel <rubensapalo97@gmail.com>
2025-08-26 15:39:19 +0100

    Hystrix fault tolerance

--- hr-payroll/src/main/java/com/rudev/hr_payroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/rudev/hr_payroll/resources/PaymentResource.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.ribbon.proxy.annotation.Hystrix;
 import com.rudev.hr_payroll.entities.Payment;
 import com.rudev.hr_payroll.services.PaymentService;
 
@@ -17,12 +19,19 @@ public class PaymentResource {
 	@Autowired
 	private PaymentService service;
 	
+	
+	@HystrixCommand(fallbackMethod = "getPaymentFallback")
 	@GetMapping(value = "/{workerId}/days/{days}")
 	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days){
 		
 		Payment payment = service.getPayment(workerId, days);
 		return ResponseEntity.ok(payment);
 		
+	}
+	
+	public ResponseEntity<Payment> getPaymentFallback(Long workerId, Integer days) {
+		Payment payment = new Payment("Erro no serviço, fallback ativado!", 0.0, days);
+		return ResponseEntity.ok(payment);
 	}
 
 }
